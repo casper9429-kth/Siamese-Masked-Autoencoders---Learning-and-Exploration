6c5a88809b9b28b55c2076812dbf59fd47b5f407
casper <cas.aug.sav@gmail.com>
2023-11-10 23:18:16 +0100

    working on training loop

--- src/pretranining.py
+++ src/pretranining.py
@@ -1,109 +1,164 @@
-## Standard libraries
 import os
-import numpy as np
+import sys
+# Get script directory
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 import math
+from typing import Iterable
+import torch
+import utils.misc as misc
+import utils.lr_sched as lr_sched
+import argparse
+import uuid
+from pathlib import Path
+import datetime
 import json
-from functools import partial
-from PIL import Image
-from collections import defaultdict
-
-
-## Imports for plotting
-import matplotlib.pyplot as plt
-plt.set_cmap('cividis')
-from IPython.display import set_matplotlib_formats
-set_matplotlib_formats('svg', 'pdf') # For export
-from matplotlib.colors import to_rgb
-import matplotlib
-matplotlib.rcParams['lines.linewidth'] = 2.0
-import seaborn as sns
-sns.reset_orig()
-
-## tqdm for progress bars
-from tqdm.auto import tqdm
-
-## To run JAX on TPU in Google Colab, uncomment the two lines below
-# import jax.tools.colab_tpu
-# jax.tools.colab_tpu.setup_tpu()
-
-## JAX
-import jax
-import jax.numpy as jnp
-from jax import random
-# Seeding for random operations
-main_rng = random.PRNGKey(42)
-
-## Flax (NN in JAX)
-import flax
-from flax import linen as nn
-from flax.training import train_state, checkpoints
-
-## Optax (Optimizers in JAX)
-import optax
-
-## PyTorch
-import torch
-import torch.utils.data as data
+import numpy as np
+import os
+import time
+from pathlib import Path
+import torch.backends.cudnn as cudnn
 from torch.utils.tensorboard import SummaryWriter
-import torchvision
-from torchvision import transforms
-from torchvision.datasets import FashionMNIST, MNIST, CIFAR10
-
-# Path to the folder where the datasets are/should be downloaded (e.g. CIFAR10)
-DATASET_PATH = "./data/MovingMNIST"
-
-
-import torch
-from torchvision.datasets import MovingMNIST
-
-dataset = MovingMNIST(root=DATASET_PATH,download=True)
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import timm
+#assert timm.__version__ == "0.3.2"  # version check
+import timm.optim.optim_factory as optim_factory
+from utils.misc import NativeScalerWithGradNormCount as NativeScaler
+import model
+from torchvision.datasets import Kinetics
 
 
-print(dataset.data.shape)
+def get_params_dict(path=SCRIPT_DIR+"/pretraining_params.json"):
+    try:
+        with open(path, "r") as f:
+            params = json.load(f)
+    except FileNotFoundError:
+        raise FileNotFoundError("No params file found at {}".format(path))
 
-# split into train and test, random split
-## Create a random permutation of indices
-dataset_data = dataset.data[torch.randperm(dataset.data.shape[0])]
-# Split the indices into train and test
-train_data = dataset_data[:8000]
-test_data = dataset_data[8000:]
-
-# Transform to JAX arrays
-train_data = jnp.array(train_data)
-test_data = jnp.array(test_data)
-
-# Find mean and std of the train dataset
-mean = jnp.mean(train_data,axis=(0,1,2,3,4))
-std = jnp.std(train_data,axis=(0,1,2,3,4))
-
-# Normalize the train and test dataset
-train_data = (train_data - mean)/std
-test_data = (test_data - mean)/std
-
-# print the mean and std of the train dataset
-print("Train data mean: ",train_data.mean())
-print("Train data std: ",train_data.std())
-
-# For each frame, sample one value in range 0.0-0.5
-t1 = random.uniform(main_rng,shape=(train_data.shape[0],1),minval=0.0,maxval=0.5)
-t1 = t1*20
-
-# For each frame, sample one value in range 0.5-1.0
-t2 = random.uniform(main_rng,shape=(train_data.shape[0],1),minval=0.0,maxval=0.5)
-t2 = t2*20 +t1
-
-# Make both t1 and t2 into integers
-t1 = t1.astype(int)
-t2 = t2.astype(int)
-
-# # Plot train data at index t1 and t2 for 10 random samples
-# fig,ax = plt.subplots(10,2,figsize=(10,20))
-# for i in range(10):
-#     ax[i,0].imshow(train_data[i,t1[i,0],0])
-#     ax[i,1].imshow(train_data[i,t2[i,0],0])
-#     ax[i,0].axis("off")
-#     ax[i,1].axis("off")
-# plt.show()
+    return params
 
 
 
+class Pretraining:
+    def __init__(self, params,debug=True):
+        """
+        Initializes a Pretraining object for a Siamese Masked Autoencoder.
+
+        Args:
+            params (dict): A dictionary containing the parameters for pretraining.
+            debug (bool, optional): Whether to print debug information. Defaults to True.
+        """
+        self.params = params
+        self.debug = debug
+
+        # Print Info
+        self.print_debug("Initializing pretraining...")
+        self.print_debug("Debug mode: {}".format(self.debug))
+        self.print_debug("Params:")
+        for key, value in params.items():
+            self.print_debug("\t{}: {}".format(key, value))
+            #setattr(self, key, value) 
+        
+        # Set device
+        try:
+            self.device = torch.device(self.params["device"])
+        except Exception as e:
+            self.print_debug("Error setting device: {}".format(e))
+            self.print_debug("Setting device to cuda if available, else cpu")
+            self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        # fix the seed for reproducibility
+        seed = self.params["seed"] #+ misc.get_rank()
+        torch.manual_seed(seed)
+        np.random.seed(seed)
+
+        # Set cudnn benchmarking
+        if self.device.type == "cuda":
+            cudnn.benchmark = True
+
+        # Load loss scaler
+        self.loss_scaler = NativeScaler()
+
+        # Model parameters
+        self.model = None
+        self.optimizer = None
+        self.transform = None
+        self.dataset = None
+
+        ## Variables for state management
+        self.data_loaded = False
+        self.transform_loaded = False        
+        self.model_loaded = False
+        self.optimizer_loaded = False
+
+    def load_transform(self, transform):
+        """
+        Loads a transform for the dataset.
+
+        Args:
+            transform (torchvision.transforms): A torchvision transform.
+        """
+        self.transform = transform
+        self.transform_loaded = True
+
+    def load_dataset(self, dataset):
+        """
+        Loads a dataset for pretraining.
+
+        Args:
+            dataset (torchvision.datasets): A torchvision dataset.
+        """
+        self.dataset = dataset
+        self.data_loaded = True
+
+    def load_model(self, model):
+        """
+        Loads a model for pretraining.
+
+        Args:
+            model (torch.nn.Module): A torch model.
+        """
+        self.model = model
+        self.model_loaded = True
+
+    def load_optimizer(self, optimizer):
+        """
+        Loads an optimizer for pretraining.
+
+        Args:
+            optimizer (torch.optim): A torch optimizer.
+        """
+        self.optimizer = optimizer
+        self.optimizer_loaded = True
+        
+    def prepare_for_training(self):
+        if not self.transform_loaded:
+            self.print_debug("No new dataset transform loaded. Using already loaded transform in dataset if available.")
+        if not self.data_loaded:
+            raise ValueError("Dataset not loaded. Please load a dataset before preparing for training. A default dataset can be loaded with load_default_dataset().")
+        if not self.model_loaded:
+            raise ValueError("Model not loaded. Please load a model before preparing for training. A default model can be loaded with load_default_model().")
+        if not self.optimizer_loaded:
+            self.print_debug("No new optimizer loaded. Using default optimizer.")
+
+        # Add transform to dataset if not already present
+        if self.transform is not None:
+            self.dataset.transform = self.transform
+
+        # Set up data loader
+
+
+
+    def print_debug(self, msg):
+        if self.debug:
+            print(msg)
+
+
+def main():
+    params = get_params_dict()
+    pretraining = Pretraining(params)
+    datasets = Kinetics()
+    datasets.split('train')
+
+if __name__ == "__main__":
+    main()
---
+++ src/utils/lr_sched.py
@@ -0,0 +1,19 @@
+# This code is based on or includes code from the Facebook Research MAE repository.
+# Original source: https://github.com/facebookresearch/mae
+# Licensed under CC-BY-NC 4.0 (Creative Commons Attribution-NonCommercial 4.0 International License).
+
+import math
+
+def adjust_learning_rate(optimizer, epoch, args):
+    """Decay the learning rate with half-cycle cosine after warmup"""
+    if epoch < args.warmup_epochs:
+        lr = args.lr * epoch / args.warmup_epochs 
+    else:
+        lr = args.min_lr + (args.lr - args.min_lr) * 0.5 * \
+            (1. + math.cos(math.pi * (epoch - args.warmup_epochs) / (args.epochs - args.warmup_epochs)))
+    for param_group in optimizer.param_groups:
+        if "lr_scale" in param_group:
+            param_group["lr"] = lr * param_group["lr_scale"]
+        else:
+            param_group["lr"] = lr
+    return lr

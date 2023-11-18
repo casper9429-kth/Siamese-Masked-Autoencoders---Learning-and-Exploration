89c7ca93481f890c25817e5732b3ff18cb3f3738
friso.dekruiff <frisodekruiff@kpnmail.nl>
2023-11-18 16:59:05 +0100

    implement dataloader for the kinetics validation dataset and fixed the bugs with the video length

--- src/data.py
+++ src/data.py
@@ -15,10 +15,11 @@ from torchvision.transforms import Compose, RandomResizedCrop,RandomHorizontalFl
 from torch.utils.data import DataLoader
 
 
-
 class PreTrainingDataset(Dataset):
+    # [test_dataset]: data_dir = ./test_dataset/* 
+    # [kinetics]: data_dir = ./data/Kinetics/train/*/*
     def __init__(self, data_dir = "./test_dataset/*",n_per_video = 2,frame_range = (4,48),patch_size = (16,16,3),target_size = (224,224),scale = (0.5,1),horizontal_flip_prob = 0.5):
-        self.data_paths = sorted(glob.glob(data_dir))
+        self.data_paths = glob.glob(data_dir)
         self.n_per_video = n_per_video
         self.frame_range = frame_range
         self.patch_size = patch_size
@@ -39,12 +40,8 @@ class PreTrainingDataset(Dataset):
 
         # Make sure video is long enough or not to short
         # If nr_frames is 0, then video is corrupted and we skip it
-        if nr_frames == 0 or nr_frames < self.frame_range[1]+1:
-            # Check that idx is not the last video
-            if idx == self.__len__()-1:
-                raise StopIteration
-            else:
-                return self.__getitem__(idx + 1) # TODO: Check that we don't double count videos
+        if nr_frames < self.frame_range[1]+1:
+            return self.__getitem__(idx + 1) 
 
         # Choose random frames
         idx_f1 = np.random.choice(np.arange(0,nr_frames-self.frame_range[1]), size=self.n_per_video, replace=False)
@@ -56,8 +53,6 @@ class PreTrainingDataset(Dataset):
 
         # Loop over number of frames per video
         for i in range(self.n_per_video):
-            if idx_f2[i] >= nr_frames:
-                idx_f2[i] = np.random.choice(np.arange(idx_f1+4,nr_frames))
             vid_capture.set(cv2.CAP_PROP_POS_FRAMES, idx_f1[i])
             _, f1 = vid_capture.read()
             vid_capture.set(cv2.CAP_PROP_POS_FRAMES, idx_f2[i])
@@ -82,7 +77,55 @@ class PreTrainingDataset(Dataset):
         f2s = np.concatenate(f2s,axis=0)
         # Shape f1s, f2s is [n_per_video,H,W,C] 
         return f1s,f2s
+    
 
+class ValidationKineticsDataset(Dataset):
+    # TODO: change the input to the correct datapath
+    def __init__(self, data_dir = "./test_dataset/*",n_frames=8,frame_gap = 4, patch_size = (16,16,3),target_size = (224,224), scale = (0.5,1),horizontal_flip_prob = 0.5):
+        self.data_paths = glob.glob(data_dir)
+        self.n_frames = n_frames
+        self.frame_gap = frame_gap
+        self.patch_size = patch_size
+        self.target_size = target_size
+        self.scale = scale
+        self.horizontal_flip_prob = horizontal_flip_prob
+        self.transform = Compose([RandomResizedCrop(size=target_size,scale = scale, antialias=True),RandomHorizontalFlip(p=horizontal_flip_prob)])
+    
+    def __len__(self):
+        return len(self.data_paths)
+
+    def __getitem__(self, idx):
+        # Open video
+        vid_capture = cv2.VideoCapture(self.data_paths[idx])
+
+        # Get length of video
+        nr_frames = int(vid_capture.get(7))
+
+        # Make sure video is long enough or not to short
+        if nr_frames < self.n_frames*self.frame_gap + 1:
+            return self.__getitem__(idx + 1) 
+
+        # Select indeces 
+        idxs = self.frame_gap*np.arange(self.n_frames)
+        # Create empty lists to store frames
+        fs = []
+
+        # Loop over number of frames per video
+        for i in range(self.n_frames):
+            vid_capture.set(cv2.CAP_PROP_POS_FRAMES, idxs[i])
+            _, f = vid_capture.read()
+            f = np.moveaxis(cv2.cvtColor(f, cv2.COLOR_BGR2RGB),-1,0)
+            if self.transform:
+                f = torch.from_numpy(f)
+                ft = self.transform(f)
+                ft = ft.float()
+                ft_mean = torch.mean(ft, dim=(1, 2))
+                ft_std = torch.std(ft, dim=(1, 2))
+                f1_norm = torch.moveaxis((ft- ft_mean.view(3, 1, 1)) / ft_std.view(3, 1, 1),0,2)
+                fs.append(f1_norm.unsqueeze(0).numpy())
+        fs = np.concatenate(fs,axis=0)
+        # Shape f1s, f2s is [n_per_video,H,W,C] 
+        return fs
 
 def main():
     dataset = PreTrainingDataset()

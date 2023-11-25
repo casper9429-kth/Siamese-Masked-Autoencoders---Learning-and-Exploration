77830ea1c18fd3df9d2928f9f2ef2274580118c3
friso.dekruiff <frisodekruiff@kpnmail.nl>
2023-11-25 15:46:09 +0100

    latest

--- src/fine_tuning.py
+++ src/fine_tuning.py
@@ -1,27 +1,34 @@
+import jax
 import numpy as np
 from sklearn.metrics import f1_score,jaccard_score
 import model
+import data
+from torch.utils.data import DataLoader
+import torch
 
 class FineTuner():
     def __init__(self,pretrained_model):
         self.pretrained_model = pretrained_model
-        self.davis_dataloader = 
-        self.jhmdb_dataloader =
-        self.vip_dataloader = 
+        self.data_loader = DataLoader(data.PreTrainingDataset(),batch_size =4)
+        self.rng = jax.random.PRNGKey(0)
+        init_batch_f1, init_batch_f2 = next(iter(self.data_loader))
+        print(init_batch_f1.shape)
+        print(init_batch_f1.flatten(0,1).shape)
+        self.params = pretrained_model.init(self.rng,init_batch_f1.flatten(0,1).numpy())
 
-
-    def label_propagation():
+    def label_propagation(m):
+                
+        for i in range(m):
+            return None
         
-        for i in range(m)
-
-
+    def test_dataset(self,top_k=7, queue_length=20,neighborhood_size=20):
+        for i, [f1s,f2s] in enumerate(self.data_loader):
+            print(f1s.shape)
 
     def DAVIS_2017(self,top_k=7, queue_length=20,neighborhood_size=20):
         
         for i, data in enumerate(self.davis_dataloader):
-            embed = apply_model(data)
-            pred = label_propagation(embed,f1)
-            
+            return None
 
         return None
     
@@ -36,10 +43,9 @@ class FineTuner():
             return None
 
 def main():
-    model = model.SiamMAE()
-    
-
-
+    fine_tuner_model = model.FineTuneSiamMAE()
+    fine_tuner = FineTuner(fine_tuner_model)
+    fine_tuner.test_dataset()
 
 if __name__ == '__main__':
     main()
--- src/model.py
+++ src/model.py
@@ -192,9 +192,9 @@ class SiamMAE(nn.Module): # For pre training
         return loss
 
 
-class fine_SiamMAE(nn.Module):
+class FineTuneSiamMAE(nn.Module):
     """ 
-        Siamese Masked Autoencoder with VisionTransformer backbone.
+        Light Weight Siamese Masked Autoencoder with VisionTransformer backbone, just the encoder part.
     """
     img_size : int = 224
     patch_size : int = 16
@@ -203,10 +203,6 @@ class fine_SiamMAE(nn.Module):
     depth : int = 24
     encoder_hidden_dim : int = 1
     num_heads : int = 16
-    decoder_embed_dim : int = 512
-    decoder_depth : int = 8
-    decoder_hidden_dim : int = 1
-    decoder_num_heads : int = 16
     mask_ratio : float = 0.95
     hparams : OmegaConf = None
     def setup(self):
@@ -220,156 +216,43 @@ class fine_SiamMAE(nn.Module):
 
         # cls token will be appended to patch embeddings
         self.cls_token = self.param("cls_token", nn.initializers.normal(stddev=0.02), (1, 1, self.embed_dim))
-        # self.cls_token = self.param("cls_token", nn.initializers.normal(stddev=0.02), (1, 1, self.embed_dim))
         # position embeddings will be added to the patch embeddings (we'll use sin-cos-distance)
         batch_size = int(num_patches**.5)
-        #self.pos_embed = self.param("pos_embed", self.sincos_pos_embed, (1, num_patches+1, self.embed_dim)) # TODO: no grad!
         self.pos_embed = self.param("pos_embed", self.sincos_pos_embed, (1, batch_size, self.embed_dim)) # TODO: no grad!
-
-
         self.encoder_blocks  = [
             Encoder(self.embed_dim, self.num_heads, self.encoder_hidden_dim) for _ in range(self.depth)
         ]
-
         self.norm = nn.LayerNorm()
 
-        # ----------------------------------- Decoder -----------------------------------
-        # embedding for decoder is just a linear layer applied to the output of the encoder
-        self.decoder_embed = nn.Dense(self.decoder_embed_dim)
-
-        self.mask_token = self.param("mask_token", nn.initializers.normal(stddev=0.02), (1, 1, self.decoder_embed_dim))
-
-        self.decoder_pos_embed = self.param("decoder_pos_embed", self.sincos_pos_embed, (1, batch_size, self.decoder_embed_dim))
-
-        self.decoder_blocks = [
-            CrossSelfDecoder(self.decoder_embed_dim, self.decoder_num_heads, self.decoder_hidden_dim) for _ in range(self.decoder_depth)
-        ]
-
-        self.decoder_norm = nn.LayerNorm()
-        self.decoder_pred = nn.Dense(self.patch_size**2 * self.in_chans, kernel_init=nn.initializers.xavier_uniform())
-
     def sincos_pos_embed(self, key, shape):
-        #_, N, embed_dim = shape[0], shape[1], shape[2]
         _, grid_size, embed_dim = shape[0], shape[1], shape[2]
-        
-        # make static grid_size 
-        #grid_size = int((N-1)**.5)
-        
         pos_embed = get_2d_sincos_pos_embed(embed_dim,grid_size , cls_token=True)
         return pos_embed[None, :, :]
 
-
-    def random_mask(self, key, x):
-        """
-            Mask out patches of the input image given a mask ratio.
-        """
-        B, N, D = x.shape
-        #num_keep = jnp.int32(N * (1-mask_ratio))
-
-        noise = random.uniform(key, shape=(B, N))
-
-        ids_shuffle = jnp.argsort(noise, axis=1)
-        ids_restore = jnp.argsort(ids_shuffle, axis=1)
-
-        ids_keep = ids_shuffle[:, :self.num_keep]
-        x_masked = jnp.take_along_axis(x, ids_keep[:, :, None], axis=1)
-
-        mask = jnp.ones((B, N))
-        mask = mask.at[:, :self.num_keep].set(0)
-
-        mask = jnp.take_along_axis(mask, ids_restore, axis=1)
-        
-        return x_masked, mask, ids_restore
-
-    def forward_encoder(self, f1, f2):
+    def forward_encoder(self, f1):
         """
             Forward pass through encoder.
             Expected dimensions for f1 and f2: n_batch x C x H x W
         """
-        # patch embeddings
         f1 = self.patch_embed(f1) # n_batch x N x embed_dim
-        f2 = self.patch_embed(f2)
-
         f1 = f1 + self.pos_embed[:, 1:, :]
-        f2 = f2 + self.pos_embed[:, 1:, :]
 
-        # mask second frame
-        key = random.key(12)
-        f2, mask, restore_ids = self.random_mask(key, f2)
-
-        # append cls token
         cls_token = self.cls_token + self.pos_embed[0, :1, :]
         cls_token = jnp.tile(cls_token, (f1.shape[0], 1, 1))
         f1 = jnp.concatenate((cls_token, f1), axis=1)
-        f2 = jnp.concatenate((cls_token, f2), axis=1)
 
-        # now inputs are ready:
-        # apply encoder blocks
         for block in self.encoder_blocks:
             f1 = block(f1)
-            f2 = block(f2)
-        
-        
-
         f1 = self.norm(f1)
-        f2 = self.norm(f2)
 
-        return f1, f2, mask, restore_ids
-    
+        return f1
 
-    def forward_decoder(self, x1, x2, ids_restore):
-        """
-            Forward pass through decoder.
-            Expected dimensions for x1 and x2: n_batch x N x D_ec
-        """
-        # embed encoder outputs (just linear layer)
-        x1 = self.decoder_embed(x1) # should the decoder embeddings be different for f1 and f2?
-        x2 = self.decoder_embed(x2) # B x N x D_dc
-
-        # add mask tokens to x2
-        mask_tokens = jnp.tile(self.mask_token,(x2.shape[0], ids_restore.shape[1] + 1 - x2.shape[1], 1))
-        x_ = jnp.concatenate((x2[:, 1:, :], mask_tokens), axis=1)
-        x_ = jnp.take_along_axis(x_, jnp.tile(ids_restore[:, :, None], (1, 1, x2.shape[2])), axis=1)
-        x2 = jnp.concatenate((x2[:, :1, :], x_), axis=1)
-
-        # add position embeddings (just to x2? if not, should they be different?)
-        # x1 = x1 + self.decoder_pos_embed
-        x2 = x2 + self.decoder_pos_embed
-        
-        # apply decoder
-        for block in self.decoder_blocks:
-            x2 = block(x1, x2)
-
-        x = self.decoder_norm(x2)
-        pred = self.decoder_pred(x)
-
-        # remove cls token
-        pred = pred[:, 1:, :]
-
-        return pred    
-
-    def __call__(self, frames1, frames2, mask_ratio):
+    def __call__(self, frames1):
         """
             Forward pass through the whole network.
         """
-        frames1_enc, frames2_enc, mask, ids = self.forward_encoder(frames1, frames2)
-        pred = self.forward_decoder(frames1_enc, frames2_enc, ids)
-
-        return pred, mask
-    
-    def loss(self, frames, pred, mask):
-        """
-            Calculate the loss.
-        """
-        target = patchify(frames, self.patch_size)
-
-        loss = (pred - target)**2
-        loss = jnp.mean(loss, axis=-1)
-
-        loss = (loss * mask).sum() / mask.sum() # calculate loss only of masked patches
-        
-        return loss
-
+        f1_embed = self.forward_encoder(frames1)
+        return f1_embed
 
 class PatchEmbed(nn.Module):
     """
@@ -459,3 +342,6 @@ class CrossSelfDecoder(nn.Module):
             linear_out = l(linear_out)
         x = norm_x + linear_out
         return x
+    
+
+    

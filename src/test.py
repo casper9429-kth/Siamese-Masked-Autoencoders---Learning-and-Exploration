768945ed36e60d27a460b5d611e7197ce6102c42
friso.dekruiff <frisodekruiff@kpnmail.nl>
2023-12-05 17:49:53 +0100

    latest

--- src/test.py
+++ src/test.py
@@ -1,35 +1,16 @@
-from jax import random
-import flax.linen as nn
-import jax
-import jax.numpy as jnp
-import torch
-
+import jax 
 from model import SiamMAE
+from data import PreTrainingDataset
+from torch.utils.data import DataLoader
 
+def test():
+    model = SiamMAE()
+    rng = jax.random.PRNGKey(42)
+    dataset = PreTrainingDataset()
+    dataloader = DataLoader(dataset,batch_size =4,shuffle=False)
+    f1s , f2s = next(iter(dataloader))
+    f1s = f1s.reshape((f1s.shape[0]*f1s.shape[1],f1s.shape[2],f1s.shape[3],f1s.shape[4]))
+    f2s = f2s.reshape((f2s.shape[0]*f2s.shape[1],f2s.shape[2],f2s.shape[3],f2s.shape[4]))
+    params = model.init(rng,f1s,f2s)
 
-def test_me(x):
-    mask_token = torch.zeros(1, 1, 512)
-    ids_restore = torch.ones((2, 196), dtype=torch.int64)
-    mask_tokens = mask_token.repeat(x.shape[0], ids_restore.shape[1] + 1 - x.shape[1], 1)
-    x_ = torch.cat((x[:, 1:, :], mask_tokens), dim=1)  # no cls token
-    x_ = torch.gather(x_, dim=1, index=ids_restore.unsqueeze(-1).repeat(1, 1, x.shape[2]))  # unshuffle
-    x = torch.cat([x[:, :1, :], x_], dim=1)  # append cls token
-
-    
-if __name__=="__main__":
-    x = random.uniform(random.key(9), (2,3,224,224))
-    y = random.uniform(random.key(123), (2,3,224,224))
-
-
-    z = torch.ones((2,196,512))
-    
-    model = SiamMAE(depth=1)
-    vars = model.init(random.key(0), x, y, 0.9)
-    pred, mask = model.apply(vars, x, y, 0.9)
-    print(pred.shape)
-    loss = model.loss(x, pred, mask)
-    print(loss)
-
-    # print(jax.tree_map(jnp.shape, vars)["params"].keys())
-
-    
+test()
